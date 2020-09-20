d91e2e6d3c238568a21f9e92e5c4a6ae4e8eef58
dpo <267046@via.dk>
2020-09-20 10:01:44 +0200

    installing needed packages and deleting other

--- BookAPI/Models/Review.cs
+++ BookAPI/Models/Review.cs
@@ -18,9 +18,13 @@ namespace BookAPI.Models
         public string Headline { get; set; }
 
         [Required]
-        [StringLength(2000, MinimumLength = 50, ErrorMessage = "ReviewText must be between 50 and 2000 characters")]
+        [StringLength(2000, MinimumLength = 50, ErrorMessage = "Review must be between 50 and 2000 characters")]
         public string ReviewText { get; set; }
+
+        [Required]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5 stars")]
         public int Rating { get; set; }
+
         public virtual Reviewer Reviewer { get; set; }
         public virtual Book Book { get; set; }
     }
