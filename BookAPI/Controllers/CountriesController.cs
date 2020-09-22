2398f498ec99bc550cd151e64892d97ff2309357
dpo <267046@via.dk>
2020-09-22 12:59:55 +0200

    creating country DTO and finishing getCountries API

--- BookAPI/Controllers/CountriesController.cs
+++ BookAPI/Controllers/CountriesController.cs
@@ -1,4 +1,5 @@
-﻿using BookAPI.Services;
+﻿using BookAPI.DTOs;
+using BookAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -20,11 +21,25 @@ namespace BookAPI.Controllers
 
         //api/countries
         [HttpGet]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<CountryDto>))]
         public IActionResult GetCountries()
         {
             var countries = _countryRepository.GetCountries().ToList();
 
-            return Ok(countries);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var countriesDto = new List<CountryDto>();
+            foreach (var country in countries)
+            {
+                countriesDto.Add(new CountryDto
+                {
+                    Id = country.Id,
+                    Name = country.Name
+                });
+            }
+            return Ok(countriesDto);
         }
     }
 }
