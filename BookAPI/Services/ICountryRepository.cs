28c1fbfb50b46546e003872c9199aff75290e78e
dpo <267046@via.dk>
2020-09-22 09:46:56 +0200

    cerating ICountryRepository interface

---
+++ BookAPI/Services/ICountryRepository.cs
@@ -0,0 +1,19 @@
+﻿using BookAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Services
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetCountries();
+
+        Country GetCountry(int countryId);
+
+        Country GetCountryOfAnAuthor(int authorId);
+
+        ICollection<Author> GetAuthorsFromACountry(int countryId);
+    }
+}
