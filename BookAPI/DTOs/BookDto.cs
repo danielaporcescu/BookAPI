f675c46b2cd6cd1682f24f8fcbfa6940d20b428a
danielaporcescu <danielaporcescu1@gmail.com>
2020-09-22 15:37:22 +0200

    implemented remaining dto classes AuthorDto, BookDto, ReviwDto, ReviewerDto

---
+++ BookAPI/DTOs/BookDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.DTOs
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public int Isbn { get; set; }
+        public string Title { get; set; }
+        public DateTime? DatePublished { get; set; }
+    }
+}
---
+++ BookAPI/DTOs/ReviewerDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.DTOs
+{
+    public class ReviewerDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastNamme { get; set; }
+    }
+}
