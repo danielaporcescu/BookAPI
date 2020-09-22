201a2a0fbb20a55d7c927ed4abf84d8d14e4ecb6
dpo <267046@via.dk>
2020-09-22 10:39:06 +0200

    cerating get countries API

---
+++ BookAPI/Controllers/CountriesController.cs
@@ -0,0 +1,30 @@
+﻿using BookAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : Controller
+    {
+        private ICountryRepository _countryRepository;
+
+        public CountriesController(ICountryRepository countryRepository)
+        {
+            _countryRepository = countryRepository;
+        }
+
+        //api/countries
+        [HttpGet]
+        public IActionResult GetCountries()
+        {
+            var countries = _countryRepository.GetCountries().ToList();
+
+            return Ok(countries);
+        }
+    }
+}
