f36cd6384decc9dbf47b470223ebfe65ed0c32e1
danielaporcescu <danielaporcescu1@gmail.com>
2020-09-26 18:16:57 +0200

    implemented BookRepository

--- BookAPI/Repositories/BookRepository.cs
+++ BookAPI/Repositories/BookRepository.cs
@@ -1,44 +1,74 @@
 ﻿using BookAPI.Models;
+using Microsoft.EntityFrameworkCore.Internal;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BookAPI.Services
 {
     public class BookRepository : IBookRepository
     {
+        private readonly BookDbContext _bookContext;
+
+        public BookRepository(BookDbContext bookContext)
+        {
+            _bookContext = bookContext;
+        }
+
         public bool BookExistsById(int bookId)
         {
-            throw new NotImplementedException();
+            return _bookContext.Books
+                 .Any(b => b.Id == bookId);
         }
 
         public bool BookExistsByISBN(string bookIsbn)
         {
-            throw new NotImplementedException();
+            return _bookContext.Books
+                .Any(b => b.Isbn == bookIsbn);
         }
 
         public Book GetBookById(int bookId)
         {
-            throw new NotImplementedException();
+            return _bookContext.Books
+                .Where(b => b.Id == bookId)
+                .FirstOrDefault();
         }
 
         public Book GetBookByISBN(string bookIsbn)
         {
-            throw new NotImplementedException();
+            return _bookContext.Books
+                .Where(b => b.Isbn == bookIsbn)
+                .FirstOrDefault();
         }
 
         public decimal GetBookRating(int bookId)
         {
-            throw new NotImplementedException();
+            var reviews = _bookContext.Reviews
+                 .Where(b => b.Book.Id == bookId);
+
+            if (reviews.Count() <= 0)
+                return 0;
+
+            var ratingAverage = ((decimal)reviews.Sum(r => r.Rating) / reviews.Count());
+
+            return ratingAverage;
         }
 
         public ICollection<Book> GetBooks()
         {
-            throw new NotImplementedException();
+            return _bookContext.Books
+                 .OrderBy(b => b.Title)
+                 .ToList();
         }
 
         public bool IsDuplicateISBN(int bookId, string bookIsbn)
         {
-            throw new NotImplementedException();
+            var book = _bookContext.Books
+                .Where(b => b.Id != bookId
+                && b.Isbn.Trim().ToUpper() == bookIsbn.Trim().ToUpper())
+                .FirstOrDefault();
+
+            return book != null;
         }
     }
 }
